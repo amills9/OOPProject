fb740113820d444a7a4eface41a1f59866c33b2f
amills9 <andrew.mills56@studytafensw.edu.au>
2025-05-29 10:59:02 +1000

    Initial File Creation

---
+++ OOP2024ProTemplate/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2024ProTemplate
+{
+    public class Employee
+    {
+        //Id, firstName, lastName, typeEmployee, hourlyRate, taxthreshold
+        public int employeeID { get; set; }
+        public string? firstName { get; set; }
+
+        public string? lastName { get; set; }
+
+        public string? typeEmployee { get; set; }
+
+        public decimal hourlyRate { get; set; }
+
+        public string taxthreshold { get; set; }
+
+
+    }
+}
